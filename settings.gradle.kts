446e7f7cfb65ee58ad98c7f5d00cbbcf54a97460
Alex Haldeman <alexhldmn5@gmail.com>
2024-07-31 00:05:05 -0400

    Initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "mnthly-money"
---
+++ src/main/kotlin/com/app/mnthly_money/MnthlyMoneyApplication.kt
@@ -0,0 +1,11 @@
+package com.app.mnthly_money
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MnthlyMoneyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MnthlyMoneyApplication>(*args)
+}
