97cc331bd689150eedc0aaef6cf30293699bdc2a
Alex Haldeman <alexhldmn5@gmail.com>
2024-11-15 21:53:06 -0500

    Format spending limit as currency

--- src/main/kotlin/com/app/mnthly_money/DashboardController.kt
+++ src/main/kotlin/com/app/mnthly_money/DashboardController.kt
@@ -3,6 +3,8 @@ package com.app.mnthly_money
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.text.NumberFormat
+import java.util.*
 
 @Controller
 class DashboardController(
@@ -11,7 +13,13 @@ class DashboardController(
     @GetMapping("/")
     fun showDashboard(model: Model): String {
         val spendingLimit = spendingLimitRepository.getSpendingLimit()
-        model.addAttribute("spendingLimit", spendingLimit)
+        model.addAttribute("spendingLimit", formatCurrency(spendingLimit))
         return "index"
+    }
+
+    fun formatCurrency(amount: Number): String {
+        val locale = Locale.of("en", "US")
+        val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
+        return currencyFormatter.format(amount)
     }
 }
